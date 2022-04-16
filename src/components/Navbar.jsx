04a3acbb78c01d117e0bdeb810165e0474aeab99
SebaTorr <storresopazo@gmail.com>
2022-04-16 19:24:43 -0400

    Se agrega link al menu del Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
               </ul>
             </li>
             <li className="nav-item">
-              <a className="nav-link disabled">Disabled</a>
+              <a className="nav-link">Contáctanos</a>
             </li>
           </ul>
           <form className="d-flex">
